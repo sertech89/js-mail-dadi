930f1000594dff5a5beb677c79d96f4ef16f68c4
Sergio Nifosì <sergio.nifosi@gmail.com>
2022-03-15 16:10:29 +0100

    gioco dei dadi

---
+++ js_Gioco-dei-dati/asset/main.js
@@ -0,0 +1,45 @@
+
+
+// Gioco dei dadi
+
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer
+
+// Stabilire il vincitore, in base a chi fa il punteggio più alto
+
+
+let bottone = document.getElementById(`btn_start`);
+
+let risultato = document.getElementById(`result`);
+
+
+
+
+
+
+bottone.addEventListener(`click`,
+
+   function(){
+
+      let dadoPlayer = Math.floor( (Math.random()  * 6) + 1);
+      let dadoPc = Math.floor( (Math.random() * 6) + 1);
+
+      document.getElementById(`player`).innerHTML = dadoPlayer;
+      document.getElementById(`pc`).innerHTML = dadoPc;
+
+      if ( dadoPlayer > dadoPc ) {
+
+          risultato.innerHTML = `u Win!`;
+
+      } else if ( dadoPlayer < dadoPc ) {
+
+          risultato.innerHTML = `u LOOOOSE!`;
+
+      } else {
+
+          risultato.innerHTML = `DraW!`;
+
+      }
+
+   }
+
+)
