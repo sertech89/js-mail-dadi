ebe0e1303335291d1d13b284b62a5ca88ae16eff
Sergio Nifosì <sergio.nifosi@gmail.com>
2022-03-15 19:45:53 +0100

    correzione POST lezione pomeridiana

--- js_Mail/asset/main.js
+++ js_Mail/asset/main.js
@@ -13,21 +13,34 @@
 
 
 
-
-let iscritti = [ "cavaniMATADOR@gmail.com", "hamsikMAREKIARO@tiscali.it", "maradonaD10S@hotmail.it", ];
+let iscritti = [ 
+    `cavaniMATADOR@gmail.com`, 
+    `hamsikMAREKIARO@tiscali.it`, 
+    `maradonaD10S@hotmail.it`, 
+    `ronaldoFENOMENO@gmail.com`,
+    `ronaldoCR7@hotmail.it`,
+    `leoMESSI@libero.it`,
+    `marioBALOTELLI@gmail.com`,
+]
 
 let inserisciMail = prompt("inserisci la tua mail");
 console.log("la mail inserita è " + inserisciMail);
 
 
 
-for (var i = 0; i < iscritti.length; i++ ) {
+let mailTrovata = false;
 
-    if ( inserisciMail === iscritti[i] ) {
-        console.log("sei già registrato!");
-        document.getElementById("risposta_in_uscita").innerHTML = `sei già registrato!`;
-    } else {
-        console.log("effettua la registrazione");
-        document.getElementById("risposta_in_uscita").innerHTML = `NON SEI REGISTRATO!!!!`;
+for ( let i=0; i < iscritti.length; i++ ) {
+
+    if ( inserisciMail == iscritti[i] ) {
+        mailTrovata = true;
     }
+}
+
+if ( mailTrovata == true ) {
+    console.log("SEI GIA' REGISTRATO!!!!");
+    document.getElementById("risposta_in_uscita").innerHTML = `SEI GIA' REGISTRATO!!!!!`;
+} else {
+    console.log("NON SEI REGISTRATO!!!!!");
+    document.getElementById("risposta_in_uscita").innerHTML = `NON SEI REGISTRATO!!!!!`;
 }
